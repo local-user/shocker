e60783e39135a00b5f465a8a59f3db1f49fef277
root <user.loca@gmail.com>
2015-10-10 09:39:31 +0000

    [added] extra/os/build-arch.sh

---
+++ extra/os/build-arch.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+#
+#       shocker -- enviroment build script -- arch linux
+#
+#       script assumes completion of either installation guide(s).
+#       --> https://wiki.archlinux.org/index.php/Installation_guide
+#       --> https://wiki.archlinux.org/index.php/Beginners'_guide
+#
+#
+
+
+    #
+    #   Start?
+    #
+#    read -p "Start Arch Build [y/n]: " choice
+choice=y
+    case "$choice" in 
+        y|Y )
+            echo "Build starting."
+            ;;
+        n|N )
+            echo "Build exiting."
+            exit 2
+            ;;
+        * )
+            echo "Invalid selection."
+            echo "Build exiting."
+            exit 2
+            ;;
+    esac
+    echo ""
+
+
+    #
+    #   Set Hostname
+    #
+    read -p "Hostname: " hostname
+#    echo $hostname > /etc/hostname
+#    if [ $? -gt 0 ]; then
+#        echo "Error: Unable to write hostname."
+#        exit 2
+#    fi
+    echo ""
+
+
+    #
+    #   Base Package(s)
+    #
+    echo "Installing dependencies:"
+#    pacman -Syu --noconfirm docker git vim tree
+#    if [ $? -gt 0 ]; then
+#        echo "Error: Unable to install dependencies."
+#        exit 2
+#    fi
+    echo ""
+
+
